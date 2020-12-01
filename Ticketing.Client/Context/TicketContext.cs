6e424a96f0f435b47b9db028a1f055d01a5b9101
Roberto Ajolfi <roberto.ajolfi@icubed.it>
2020-12-01 10:08:28 +0100

    Add project files.

---
+++ Ticketing.Client/Context/TicketContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Ticketing.Client.Model;
+
+namespace Ticketing.Client.Context
+{
+    public class TicketContext : DbContext
+    {
+        DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionBuilder
+        )
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("TicketDb");
+            // OPPURE
+            //string connString = config.GetSection("ConnectionStrings")["TicketDb"];
+
+            optionBuilder.UseSqlServer(connString);
+        }
+    }
+}
---
+++ Ticketing.Client/Model/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Client.Model
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public DateTime IssueDate { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Priority { get; set; }
+        public string State { get; set; }
+    }
+}
